698da605a43d43ea80e664f4d138dc5e9970a4bd
OUGO <hugo.parmentier@efrei.net>
2024-05-21 11:36:56 +0200

    Adding some complex maths problems

--- main.py
+++ main.py
@@ -20,32 +20,41 @@ from langchain.prompts import PromptTemplate
 from langchain.prompts import MessagesPlaceholder
 from langchain.prompts import HumanMessagePromptTemplate
 
-message = [SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=['question'], template="""
-    You are a helpful agent, you are here to help the user solve the problem. 
-    You are not a calculator or a  
+message = [SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[], template="""
+    You are a helpful assitant who is good in mathematics and good in solving problems.
+    Do not calculate yourself let the tool do the calculation. Call one tool at a time.
     Logically arrive at the solution, and be factual. 
-    In your answers, clearly detail the steps involved.
-    Calculate step by step with the calculator
-    After the reasoning give the final answer. Provide the response in bullet points. 
-    Question  {question} Answer""")),
-           MessagesPlaceholder(variable_name='chat_history', optional=True),
-           MessagesPlaceholder(variable_name='agent_scratchpad')]
+    In your answers, clearly detail the steps involved and give the final answer.""")),
+        MessagesPlaceholder(variable_name='chat_history', optional=True),
+        MessagesPlaceholder(variable_name='agent_scratchpad'),
+        HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['message'], template="{user_message}"))]
+
 from langchain.prompts import ChatPromptTemplate
 prompt = ChatPromptTemplate(messages=message)
 
 #print(word_problem_tool.run({"querry": "You have a fruit basket containing 5 different types of fruit: apples, oranges, bananas, grapes, and kiwis. Here's the scenario: You start with 6 apples, 8 oranges, 10 bananas, 20 grapes, and 15 kiwis. You decide to make fruit baskets for your friends. You make 3 fruit baskets, each containing an equal number of each type of fruit. After making the fruit baskets, you realize you want to add more variety. So, you go to the market and buy 4 pineapples, 3 watermelons, and 2 packs of mixed berries. Each pack of mixed berries contains 50 berries. You decide to distribute the new fruits evenly among the existing fruit baskets. Question: How many pieces of fruit are there in each of the updated fruit baskets, and how many pieces of each type of fruit do you have left over?"}))
 
-agent = setup_openai_agent(llm,[math_tool, word_problem_tool],prompt)
+agent = setup_openai_agent(llm,[math_tool],prompt)
 
-#print(agent.invoke(
-#    {"question": """You have a fruit basket containing 5 different types of fruit: apples, oranges, bananas, grapes, and kiwis.
-#
-#Here's the scenario:
-#
-#    You start with 6 apples, 8 oranges, 10 bananas, 20 grapes, and 15 kiwis.
-#    You decide to make fruit baskets for your friends. You make 3 fruit baskets, each containing an equal number of each type of fruit.
-#    After making the fruit baskets, you realize you want to add more variety. So, you go to the market and buy 4 pineapples, 3 watermelons, and 2 packs of mixed berries. Each pack of mixed berries contains 50 berries.
-#    You decide to distribute the new fruits evenly among the existing fruit baskets.
-#
-#Question: How many pieces of fruit are there in each of the updated fruit baskets, and how many pieces of each type of fruit do you have left over?"""
-#     }))
+probleme1 = """You have a fruit basket containing 5 different types of fruit: apples, oranges, bananas, grapes, and kiwis.
+
+    Here's the scenario:
+
+    You start with 6 apples, 8 oranges, 10 bananas, 20 grapes, and 15 kiwis.
+    You decide to make fruit baskets for your friends. You make 3 fruit baskets, each containing an equal number of each type of fruit.
+    After making the fruit baskets, you realize you want to add more variety. So, you go to the market and buy 4 pineapples, 3 watermelons, and 2 packs of mixed berries. Each pack of mixed berries contains 50 berries.
+    You decide to distribute the new fruits evenly among the existing fruit baskets.
+
+    Question: How many fruits are in each of the updated fruit baskets and how many pieces of each fruit in a single basket?"""
+
+probleme2 = """Consider a triangle ABC where angle A is 60∘, side AC has a length of 5 units, and side BC has a length of 7 units.
+
+    Find the length of side AB.
+    Determine the measures of angles B and C.
+    Calculate the area of triangle ABC.
+    """
+
+
+print(agent.invoke(
+    {"user_message": probleme1}
+    ))
